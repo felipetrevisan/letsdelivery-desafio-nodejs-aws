36d93ba3a8b45eb85da593d88865ebe2a081870a
Felipe Trevisan <felipejs@gmail.com>
2025-02-13 21:21:12 -0300

    initial setup

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: 'es5',
+  semi: true,
+  singleQuote: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  printWidth: 100,
+};
+
+module.exports = config;
